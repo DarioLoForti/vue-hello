e53e3f68450d6735c38667d2f15efce818b27310
DarioLoForti <lofortidario@gmail.com>
2023-12-11 15:41:44 +0100

    Bonus completed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-            message: "Welcome to Vue.js"
+            message: "Welcome to Vue.js",
+            img: "https://community-cdn-digitalocean-com.global.ssl.fastly.net/snN3rbgKF7McfuiQAKcoLWMn"
         }
     }
 }).mount("#app");
